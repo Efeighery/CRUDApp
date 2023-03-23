66adbe3658dd10715af1c16426096c006618b0fb
Efeighery <eoghanrcfeighery@gmail.com>
2023-03-23 15:44:29 +0000

    change to script

--- script.js
+++ script.js
@@ -5,62 +5,24 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const cpassword = document.getElementById('cpassword');
 
-var pass;
-
-while (pass !== "beans"){
-    pass = prompt("Guess the password to enter the application");
-
-    if(pass === "beans"){
-        break;
-    }
-}
-
-// Sign up functionality
-function signUp(e){
-    e.preventDefault();
-
-    console.log('Testing');
-
-    var username = document.getElementById('username');
-    var surname = document.getElementById('surname');
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var cpassword = document.getElementById('cpassword');
-
-    var user = {
-        username: username,
-        surname: surname,
-        email: email,
-        password: password,
-        cpassword: cpassword,
-    };
-
-    var json = JSON.stringify(user);
-    localStorage.setItem(user, json);
-
-    console.log('User added');
-}
-
-form.addEventListener('submit', event =>{
+form.addEventListener('submit', (event) =>{
     event.preventDefault();
 
-    validateInputs();
-    signUp();
-});
+    Validate();
+})
 
-const sendData = (usernameVal, sRate, Count) =>{
+const sendData = (usernameVal, sRate, Count) => {
     if(sRate === Count){
-        console.log("Hello "+usernameVal+ "; Registration complete!", "success");
+        swal("Hello " + usernameVal , "You are Registered", "success");
     }
 }
 
 const SuccessMsg = (usernameVal) => {
-    let formContr = document.getElementById('form-control');
+    let formContr = document.getElementsByClassName('form-control');
     var Count = formContr.length - 1;
-
-    for(var a = 0; a < formContr.length; a++){
-        if(formContr[a].className === "form-control success"){
-            var sRate = 0 + a;
+    for(var i = 0; i < formContr.length; i++){
+        if(formContr[i].className === "form-control success"){
+            var sRate = 0 + i;
             console.log(sRate);
             sendData(usernameVal, sRate, Count);
         }
@@ -70,82 +32,81 @@ const SuccessMsg = (usernameVal) => {
     }
 }
 
+
 const isEmail = (emailVal) =>{
     var atSymbol = emailVal.indexOf('@');
     if(atSymbol < 1) return false;
     var dot = emailVal.lastIndexOf('.');
     if(dot <= atSymbol + 2) return false;
-    // if(dot === emailVal.length -1) return false;
+    if(dot === emailVal.length -1) return false;
     return true;
 }
 
-function validateInputs(){
+function Validate(){
     const usernameVal = username.value.trim();
-    const surnameVal = surname.value.trim();
+    const lastnameVal = lastname.value.trim();
     const emailVal = email.value.trim();
     const passwordVal = password.value.trim();
     const cpasswordVal = cpassword.value.trim();
 
-
-    // username
+    //username
     if(usernameVal === ""){
-        setErrorMsg(username, 'First name can NOT be blank');
+        setErrorMsg(username, 'first name cannot be blank');
     }
-    else if (usernameVal.length <=5){
-        setErrorMsg(username, '6 characters at most');
+    else if(usernameVal.length <=2){
+        setErrorMsg(username, 'min 3 char');
     }
     else{
         setSuccessMsg(username);
     }
 
-    // last name
+    //last name
+
     if(surnameVal === ""){
-        setErrorMsg(surname, 'Surname can NOT be blank');
+        setErrorMsg(surname, 'last name cannot be blank');
     }
-    else if (surnameVal.length <=5){
-        setErrorMsg(surname, '6 characters at most');
+    else if(surnameVal.length <=2){
+        setErrorMsg(surname, 'min 3 char');
     }
     else{
         setSuccessMsg(surname);
     }
 
-    // email
+    //email
     if(emailVal === ""){
-        setErrorMsg(email, 'Email can NOT be blank');
+        setErrorMsg(email, 'email cannot be blank');
     }
-    else if (!isEmail(emailVal)){
-        setErrorMsg(email, 'Email isnt valid');
+    else if(!isEmail(emailVal)){
+        setErrorMsg(email, 'email is not valid');
     }
     else{
         setSuccessMsg(email);
     }
 
-    // password
+    //password
     if(passwordVal === ""){
-        setErrorMsg(password, 'Password can NOT be blank');
+        setErrorMsg(password, 'password cannot be blank');
     }
-    else if (passwordVal.length <=7){
-        setErrorMsg(password, '8 characters at most');
+    else if(passwordVal.length <= 7){
+        setErrorMsg(password, 'min 8 char');
     }
     else{
         setSuccessMsg(password);
     }
 
-        // Confirm password
+    //confirm password
     if(cpasswordVal === ""){
-        setErrorMsg(cpassword, 'Password can NOT be blank');
+        setErrorMsg(cpassword, 'confirm password cannot be blank');
     }
-    else if (passwordVal != cpasswordVal){
-        setErrorMsg(cpassword, 'No matches!!');
+    else if(passwordVal != cpasswordVal){
+        setErrorMsg(cpassword, 'Not Matched!');
     }
     else{
-        setSuccessMsg(cpassword, 'Registration complete');
-        // alert('Boo!');
+        setSuccessMsg(cpassword);
     }
+    SuccessMsg(usernameVal);
 
-    setSuccessMsg(usernameVal);
 }
-
 function setErrorMsg(input, errormsgs){
     const formControl = input.parentElement;
     const small = formControl.querySelector('small');
